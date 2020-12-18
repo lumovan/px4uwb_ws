1ac06fc2e4543b47b93d57974203f46ff4fc4aeb
shupx <1260736784@qq.com>
2020-12-19 00:18:21 +0800

    readme.md

---
+++ build/px4_uwb/CMakeFiles/uwb_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uwb_node.dir/src/uwb.cpp.o"
+  "/home/shupeixuan/px4uwb_ws/devel/lib/px4_uwb/uwb_node.pdb"
+  "/home/shupeixuan/px4uwb_ws/devel/lib/px4_uwb/uwb_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uwb_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/px4_uwb/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/shupeixuan/px4uwb_ws/src/px4_uwb/include".split(';') if "/home/shupeixuan/px4uwb_ws/src/px4_uwb/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "px4_uwb"
+PROJECT_SPACE_DIR = "/home/shupeixuan/px4uwb_ws/devel"
+PROJECT_VERSION = "0.0.0"
