8218127b575fe1725607250d2efa82888fc59729
shupx <1260736784@qq.com>
2020-12-19 00:19:37 +0800

    delete useless files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shupeixuan/px4uwb_ws/src
-# Build directory: /home/shupeixuan/px4uwb_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("px4_uwb")
--- build/px4_uwb/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shupeixuan/px4uwb_ws/src/px4_uwb
-# Build directory: /home/shupeixuan/px4uwb_ws/build/px4_uwb
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
